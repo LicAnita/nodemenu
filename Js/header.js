d67d8297d5dd952920135b13e87bdd1971c94532
Ana Toscano <90354141+LicAnita@users.noreply.github.com>
2024-05-21 16:58:34 -0300

    Update header.js
    Cambio de footer

--- Js/header.js
+++ Js/header.js
@@ -46,7 +46,7 @@ document.getElementById('header-content').innerHTML = `
 
 document.getElementById('footer-content').innerHTML = `
     <div class="footer">
-        <p>Creado por Anita - 2023</p>
+        <p>Creado por Ana, César, Felipe y Jonathan - 2024</p>
     </div>
 `;
 
